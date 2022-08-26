97286b158c93044649acd12641a89a6c8a3abd1c
Csiszár Tamás <CsiszarT@idomsoft.hu>
2022-08-26 12:05:31 +0200

    added public functions for test class, corrected linting problems

--- main.py
+++ main.py
@@ -8,6 +8,14 @@ class ATest:
     """Test class."""
 
     def __init__(self) -> None:
-        """Initializer of class ATest."""
-        self.df = DataFrame()
+        """Initialize of class ATest."""
+        self.dataframe = DataFrame()
         self.vec = numpy.ones(0)
+
+    def print_hello_world(self) -> None:
+        """Print 'Hello World!'."""
+        print("Hello World!")
+
+    def get_one(self) -> int:
+        """Get a one."""
+        return 1
